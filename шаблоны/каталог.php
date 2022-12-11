30d0ef074e45c08c3df448a9bd6c45e2a7832495
root <q8x8l@yandex.by>
2022-12-11 16:52:45 +0300

    правки главной
    скоректировал вывод каталога

--- шаблоны/каталог.php
+++ шаблоны/каталог.php
@@ -13,7 +13,6 @@
   include          $h2 . 'панель_сортировки_и_фильтра.php';
   if ($h0) include $h2 . 'категории_для_бренда.php';
   echo                   '<ul class="лента_каталога"></ul>';
-  // include          $h2 . 'пагинация.php';
   if ($r)  include $h2 . 'соседние_категории.php';
   include                'блоки/ноги.php';
 ?>
