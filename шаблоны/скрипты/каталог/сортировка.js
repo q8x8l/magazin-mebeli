e3ec1f748522a0c952789a8c0979ebc7d96e8d88
root <q8x8l@yandex.by>
2023-02-07 13:24:11 +0300

    Update сортировка.js
    апдейт кода

--- шаблоны/скрипты/каталог/сортировка.js
+++ шаблоны/скрипты/каталог/сортировка.js
@@ -1,8 +1,10 @@
 if ($(x0 = '.панель_сортировки_и_фильтра ').length == 1) {
-  $('body').on('click', x0 + '> *:eq(1) form > *', function() {
-    $get1 (9, 0)
-    q0 = $(this)
-    q0.parent().next().html(q0.html())
-    сортировка = '&сортировка=' + q0.text()
-  })
+  $('body').on('click', x0 + '> *:eq(1) form > *', function ()
+    {
+      $get1 (9, 0)
+      q0 = $(this)
+      q0.parent().next().html(q0.html())
+      сортировка = '&сортировка=' + q0.text()
+    }
+  );
 }
